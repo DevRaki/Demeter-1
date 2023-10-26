83d2508f831ef0a6f4f4c4b4674eddd153ee5c5c
CaterinMB <cmartinez2131@misena.edu.co>
2023-10-26 00:24:18 -0500

    Header

--- Front-End/src/components/Header.jsx
+++ Front-End/src/components/Header.jsx
@@ -14,67 +14,74 @@ const Header = () => {
 
 	return (
 		<header className="pc-header">
-			<div className="header-wrapper">
-				<div className="mr-auto pc-mob-drp">
-					<ul className="list-unstyled">
-						<li className="dropdown pc-h-item">
-							<h3><strong>DEMETER</strong></h3>
-						</li>
-					</ul>
-				</div>
-				<div className="ml-auto">
-					<ul className="list-unstyled">
-						<li className="dropdown pc-h-item">
-							<button
-								className="pc-head-link dropdown-toggle arrow-none mr-0"
-								role="button"
-								aria-haspopup="false"
-								aria-expanded="false"
-								onClick={toggleDropdown}
-							>
-								<span>
-									<span className="user-name">Samuel Rios A.</span>
-									<span className="user-desc">Administrator</span>
-								</span>
-							</button>
-							{showDropdown && (
-								<ul className="dropdown-menu dropdown-menu-right pc-h-dropdown">
-									<li className="dropdown-item">
-										<button
-											onClick={() => {
-												navigate('/');
-											}}
-										>
-											<ChromeReaderModeIcon />
-											<span>Editar perfil</span>
-										</button>
-									</li>
-									<li className="dropdown-item">
-										<button
-											onClick={() => {
-												navigate('/');
-											}}
-										>
-											<LockIcon />
-											<span>Cambio contraseña</span>
-										</button></li>
-									<li className="dropdown-item">
-										<button
-											onClick={() => {
-												navigate('/');
-											}}
-										>
-											<ExitToAppIcon />
-											<span>Logout</span>
-										</button>
-									</li>
-								</ul>
-							)}
-						</li>
-					</ul>
-				</div>
-
+			{/* <div className="header-wrapper"> */}
+			<div className="mr-auto pc-mob-drp">
+				<ul className="list-unstyled">
+					<li className="dropdown pc-h-item">
+						<h3><strong>DEMETER</strong></h3>
+					</li>
+				</ul>
 			</div>
+			<div className="ml-auto">
+				<ul className="list-unstyled">
+					<li className="dropdown pc-h-item">
+						<button
+							className="pc-head-link dropdown-toggle arrow-none mr-0"
+							role="button"
+							aria-haspopup="false"
+							aria-expanded="false"
+							onClick={toggleDropdown}
+						>
+							<span>
+								<span className="user-name">Samuel Rios A.</span>
+								<span className="user-desc">Administrator</span>
+							</span>
+						</button>
+						{showDropdown && (
+							<ul className="dropdown-menu dropdown-menu-right pc-h-dropdown">
+								<li className="dropdown-item">
+									<button
+										onClick={() => {
+											navigate('/');
+										}}
+									>
+										<i class="material-icons-two-tone">
+											<ChromeReaderModeIcon />
+										</i>
+										<span>Editar perfil</span>
+									</button>
+								</li><br />
+								<li className="dropdown-item">
+									<button
+										onClick={() => {
+											navigate('/');
+										}}
+									>
+										<i class="material-icons-two-tone">
+											<LockIcon />
+										</i>
+										<span>Cambio contraseña</span>
+									</button>
+								</li><br />
+								<li className="dropdown-item">
+									<button
+										onClick={() => {
+											navigate('/');
+										}}
+									>
+										<i class="material-icons-two-tone">
+											<ExitToAppIcon />
+										</i>
+										<span>Logout</span>
+									</button>
+								</li>
+							</ul>
+						)}
+					</li>
+				</ul>
+			</div>
+
+			{/* </div> */}
 		</header>
 	);
 };
