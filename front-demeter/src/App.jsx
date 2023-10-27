7fea5f6462afaee383275cf89cc060df55ca68cf
CaterinMB <cmartinez2131@misena.edu.co>
2023-10-26 22:53:58 -0500

    Menu&Header

--- front-demeter/src/App.jsx
+++ front-demeter/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Navbar from './Components/Navbar.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Form from './Components/Form.jsx'
 import { Supplier } from './Context/Supplier.context.jsx'
 import Login from './Pages/Login.jsx'
+
+// Menu & Header
+import Navbar from './Components/Navbar.jsx'
+import Header from './components/Header.jsx'
 
 
 function App() {
@@ -11,24 +14,16 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Supplier>
-    <Navbar/> 
-    <Routes>
-      <Route path='/supplier' element={<Form/>}/>
-      
-    </Routes>
-    </Supplier>
-    </BrowserRouter>
-   
-      
-
-   
-     
-
-  
-    
-     {/* <Login/> */}
+      <BrowserRouter>
+        <Supplier>
+          <Navbar />
+          <Header />
+          <Routes>
+            <Route path='/supplier' element={<Form />} />
+          </Routes>
+        </Supplier>
+      </BrowserRouter>
+      {/* <Login/> */}
     </>
   )
 }
